f03e007f4fbdc10c07da167b1bad44ea868a6801
mbeltran05 <165904034+mbeltran05@users.noreply.github.com>
2024-08-05 11:45:38 -0300

    script.js

--- script.js
+++ script.js
@@ -6,68 +6,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "A chuva ácida provoca inúmeras perdas ambientais para o meio. Esse fenômeno ocorre com frequência em regiões industriais devido ao:",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                a) processo de contaminação do solo pela produção industrial.
+
+                b) aumento da quantidade de vapor de água dissipado no ar.
+
+                c) conjunto de dinâmicos atmosfericos influenciadas pela água.
+    
+                d) aquecimento global gerado pela atenuação da temperatura.
+
+                e) elevado volume de concentração de poluentes na atmosfera
             },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "O Brasil enfrenta diversos problemas ambientais que prejudicam as diferentes espécies que aqui vivem. De acordo com IBGE, Très problemas ambientais são os mais Relatados No Brasil. Marque a alternativa que indica esses problemas:",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                a) Poluição do solo, poluição atmosférica e contaminação por metais pesados.
+
+                b) contaminação por metais pesados, desmatamento e caça.
+
+                c) Poluição atmosférica, queimadas e caça.
+
+                d) assoreamento, desmatamento e queimadas.
             },
-            {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "O aquecimento global, o problema ambiental mais preocupante na atualidade, é a elevação acelerada das temperaturas médias da terra. São causas desse problema exceto:",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "afirmação"
+                a) queima de combustíveis fósseis, que libera grande quantidade de gases de efeito estufa.
+            
+                b) redução das geleiras presentes nos polos do planeta e nos topes das montanhas.
+            
+                c) queimadas e desmatamento de áreas florestais.
             },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "afirmação"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "afirmação"
-            }
         ]
     },
 ];
