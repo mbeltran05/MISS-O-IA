5f464ad301d7a57cc61557b1c021523d13f0fba7
mbeltran05 <165904034+mbeltran05@users.noreply.github.com>
2024-10-21 11:15:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,41 +6,54 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "A chuva ácida provoca inúmeras perdas ambientais para o meio. Esse fenômeno ocorre com frequência em regiões industriais devido ao:",
+        enunciado: "A chuva ácida provoca inúmeras perdas ambientais para o meio. Esse fenômeno ocorre com frequência em regiões industriais devido ao",
         alternativas: [
             {
-                a) processo de contaminação do solo pela produção industrial.
-
-                b) aumento da quantidade de vapor de água dissipado no ar.
-
-                c) conjunto de dinâmicos atmosfericos influenciadas pela água.
-    
-                d) aquecimento global gerado pela atenuação da temperatura.
-
-                e) elevado volume de concentração de poluentes na atmosfera
+                texto: "a) processo de contaminação do solo pela produção industrial.",
+                afirmacao: "afirmação"
             },
+            {
+                texto: "b) aumento da quantidade de vapor de água dissipado no ar.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "c) conjunto de dinâmicos atmosfericos influenciadas pela água.",
+                afirmacao: "afirmação"
+            },
+        ]
+    },
     {
-        enunciado: "O Brasil enfrenta diversos problemas ambientais que prejudicam as diferentes espécies que aqui vivem. De acordo com IBGE, Très problemas ambientais são os mais Relatados No Brasil. Marque a alternativa que indica esses problemas:",
+        enunciado: "O Brasil enfrenta diversos problemas ambientais que prejudicam as diferentes espécies que aqui vivem. De acordo com IBGE, Três problemas ambientais são os mais relatados no Brasil. Marque a alternativa que indica esses problemas:",
         alternativas: [
             {
-                a) Poluição do solo, poluição atmosférica e contaminação por metais pesados.
-
-                b) contaminação por metais pesados, desmatamento e caça.
-
-                c) Poluição atmosférica, queimadas e caça.
-
-                d) assoreamento, desmatamento e queimadas.
+                texto: "a) Poluição do solo, poluição atmosférica e contaminação por metais pesados.",
+                afirmacao: "afirmação"
             },
+            {
+                texto: "b) Poluição atmosférica, queimadas e caça.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "c) assoreamento, desmatamento e queimadas.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
     {
         enunciado: "O aquecimento global, o problema ambiental mais preocupante na atualidade, é a elevação acelerada das temperaturas médias da terra. São causas desse problema exceto:",
         alternativas: [
             {
-                a) queima de combustíveis fósseis, que libera grande quantidade de gases de efeito estufa.
-            
-                b) redução das geleiras presentes nos polos do planeta e nos topes das montanhas.
-            
-                c) queimadas e desmatamento de áreas florestais.
+                texto: "a) queima de combustíveis fósseis, que libera grande quantidade de gases de efeito estufa.",
+                afirmacao: "afirmação"
             },
+            {
+                texto: "b) redução das geleiras presentes nos polos do planeta e nos topos das montanhas.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "c) queimadas e desmatamento de áreas florestais.",
+                afirmacao: "afirmação"
+            }
         ]
     },
 ];
